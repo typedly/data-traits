7580edc09310ab62e0f95eb502b5de0a56161839
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-12 00:31:19 +0000

    refactor(`Configurable`): enhance description and update type template

--- src/lib/configurable.interface.ts
+++ src/lib/configurable.interface.ts
@@ -1,16 +1,14 @@
 /**
- * @description The interface for configurable data types.
+ * @description The interface for configurable data types, which can optionally have a configuration object.
  * @export
  * @interface Configurable
- * @template C The type of the configuration object.
+ * @template [C=unknown] The type of the configuration object.
  */
-export interface Configurable<C = any> {
+export interface Configurable<C = unknown> {
   /**
    * @description The configuration of the `Data` instance.
    * @readonly
    * @type {?C}
    */
-  readonly configuration?: C;
-
-  
+  readonly configuration?: C;  
 }
