7a5d00f7d21095421a1e235f0ea9f8382c033fb3
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-13 06:22:05 +0000

    chore(api): update

--- src/public-api.ts
+++ src/public-api.ts
@@ -4,14 +4,40 @@
 
 export type {
   Adaptable,
+
   Cacheable,
+  Collection,
   Comparable,
+  Compressible,
   Configurable,
+
   Encryptable,
   Exportable,
+
+  Freezable,
+
+  Identifiable,
+  Indexable,
+
+  Measurable,
+  Mergeable,
+
+  Observable,
+
+  Paginated,
+  Persistable,
+  Printable,
+
+  Queryable,
+
   Resettable,
+  Revertible,
+
   Serializable,
+
   Timestamped,
   Transformable,
+
   Validatable,
+  Versionable,
 } from './lib';
